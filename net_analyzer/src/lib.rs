650e635ab109f5672fe5683f757c58284240c0b2
Ayaal Ali <ayaalali@protonmail.com>
2023-02-23 00:10:22 +0800

    Successfull trace, pass data to main as vector

--- net_analyzer/src/lib.rs
+++ net_analyzer/src/lib.rs
@@ -1,3 +1,52 @@
-pub fn scan() {
-    
+use tracert;
+use std::net::IpAddr;
+
+//pub fn test_trace() { 
+//    let ip_tracer:Result<tracert::trace::Tracer, String> = tracert::trace::Tracer::new(TEST_IP);
+//    match ip_tracer {
+//        Ok(v) => {
+//            println!("Tracer Setup Successfull");
+//            let ip_trace:Result<tracert::trace::TraceResult, String> = v.trace();
+//            match ip_trace{
+//                Ok(v) => {
+//                    for ip_node in v.nodes {
+//                        println!("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+//                        println!("Node: {}", ip_node.seq);
+//                        println!("IP Address: {}", ip_node.ip_addr);
+//                    }
+//                    println!("Test OK!");
+//                },
+//                Err(e) => println!("Trace Failed. Error: {e:?}"),
+//            }
+//        },
+//        Err(e) => println!("Tracer Setup Failed. Error: {e:?}"),
+//    }
+//}
+//
+pub fn scan(){
+
+}
+
+pub fn trace(dest_ip:IpAddr) -> Result<Vec<IpAddr>, String>{
+    let mut path_to_dest = vec![];
+    let ip_tracer:Result<tracert::trace::Tracer, String> = tracert::trace::Tracer::new(dest_ip);
+    match ip_tracer {
+        Ok(t) => {
+            println!("Tracer Setup Successfull");
+            let ip_trace:Result<tracert::trace::TraceResult, String> = t.trace();
+            match ip_trace{
+                
+                Ok(r) => {
+                    for ip_node in r.nodes {
+                        //let dummy: &mut Vec<IpAddr> = &mut vec![ip_node.ip_addr];
+                        path_to_dest.push(ip_node.ip_addr);
+                    }
+                    println!("Test OK!");
+                },
+                Err(e) => return Result::Err(e),
+            }
+        },
+        Err(e) => return Result::Err(e),
+    }
+    return Result::Ok(path_to_dest);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use std::net::{IpAddr, Ipv4Addr};
 use net_analyzer;
 
+const TEST_IP:IpAddr = IpAddr::V4(Ipv4Addr::new(172,253,118,101));
+
 fn main() {
-    println!("Hello, world!");
-    net_analyzer::scan();
+    let path = net_analyzer::trace(TEST_IP);
+    match path {
+        Ok(p) => {
+            for ip in p {
+                println!("|");
+                println!("|");
+                println!("{}", ip);
+            }
+        },
+        Err(e) => print!("{}", e),
+    }
 }
